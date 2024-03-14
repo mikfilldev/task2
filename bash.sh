39507be27d10a575d9e20e929a84a6be7ef2b54a
Mykola Filippenko <mykolafilippenkodev@gmail.com>
2024-03-14 20:07:34 +0100

    Update script: Introduce random increments in counter loop

--- bash.sh
+++ bash.sh
@@ -1,7 +1,10 @@
-#!/bin/bash 
-COUNTER=0 
-while [ $COUNTER -lt 10 ] 
-do 
-   echo The counter is $COUNTER 
-   let COUNTER=$COUNTER+1 
+#!/bin/bash
+
+COUNTER=0
+
+while [ $COUNTER -lt 20 ]; do
+    echo "The current counter value is: $COUNTER"
+    # Randomly choose between incrementing by 2 or 3
+    RANDOM_INCREMENT=$(( RANDOM % 2 + 2 ))
+    let COUNTER=$COUNTER+$RANDOM_INCREMENT
 done
